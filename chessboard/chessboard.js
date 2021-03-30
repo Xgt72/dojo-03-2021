d233e60e218119405ba4e123d7f3d184acf11f0b
Xgt <xageorget@gmail.com>
2021-03-30 11:47:52 +0200

    solution chessboard groupe 2

--- chessboard/chessboard.js
+++ chessboard/chessboard.js
@@ -1,32 +1,16 @@
 function chessboard() {
-  // on crée un tableau lettre vide
-  let lettre = [];
-  // on rempli ce tableau avec les caractères de "a" (code ASCII 97) à "h" (code ASCII 104) 
-  for (let i = 0; i < 8; i++) {
-    lettre[i] = String.fromCharCode(i + 97);
-  }
-  // console.log("Lettres: ", lettre);
+  const result = [];
+  for (let j = 97; j < 105; j++) {
+    const colonne = [];
 
-  // on crée un tableau finalTable vide
-  let finalTable = [];
-
-  // on rempli finalTable avec les tableaux des différentes colonnes
-  for (let j = 0; j < lettre.length; j++) {
-    // on crée un tableau vide column
-    let column = [];
-    // on rempli column avec le caractère lettre[i] et " - " et la valeur de i
-    for (let i = 1; i <= 8; i++) {
-      column[i - 1] = lettre[j] + " - " + i;
-      // column.push("a - " + i);
+    for (let i = 0; i < 8; i++) {
+      // colonne[i] =`${String.fromCharCode(97)} - ${i+1}`;
+      colonne.push(`${String.fromCharCode(j)} - ${i + 1}`);
     }
-    // on ajoute le tableau column à la fin de finalTable
-    finalTable.push(column);
+    result.push(colonne);
   }
-  // on retourne finalTable
-  return finalTable;
+  return result;
 }
-
-
 console.log(chessboard());
 
 module.exports = chessboard;
